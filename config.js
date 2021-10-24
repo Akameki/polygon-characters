53ff3fce672e35a02ae23033a4e920dc7ca85c68
Leung <andrew_leung@cable.comcast.net>
2021-10-24 05:32:32 -0400

    use mumbai

--- config.js
+++ config.js
@@ -1,4 +1,2 @@
-
-  export const nftmarketaddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-  export const nftaddress = "0xe7f1725E7734CE288F8367e1Bb143E90bb3F0512"
-  
+export const nftmarketaddress = "0x2AF39767935De151e2e9c1A8791210594762900C"
+export const nftaddress = "0xb738Cd9f278A83ecc469aE7d044d9D22D08C9599"
