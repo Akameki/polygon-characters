6378034315b21a08378dbe1a36c0bd326a2ccf12
Leung <andrew_leung@cable.comcast.net>
2021-12-09 04:11:02 -0500

    newbanner

--- pages/_app.js
+++ pages/_app.js
@@ -7,7 +7,7 @@ import Link from 'next/link'
 function Marketplace({ Component, pageProps }) {
   return (
     <div>
-      <nav className="border-b p-6 bg-cover">
+      <nav className="border-b p-6 page-header">
         <div className="flex mt-4">
           <div className="bg-header">
             <Link href="/">
@@ -46,60 +46,6 @@ function Marketplace({ Component, pageProps }) {
                 </a>
             </p>
           </div>
-          <svg className="svg1" viewBox='0 0 150 150'>
-            <path d='M 25,60
-                     a 20,20 1 0,0 0,80
-                     h 100
-                     a 20,20 1 0,0 0,-80
-                     a 10,10 1 0,0 -30,-20
-                     a 15,15 1 0,0 -70,20
-                     z' />
-          </svg>
-          <svg className="svg2" viewBox='0 0 105 105'>
-            <path d='M 25,60
-                     a 20,20 1 0,0 0,40
-                     h 50
-                     a 20,20 1 0,0 0,-40
-                     a 10,10 1 0,0 -15,-10
-                     a 15,15 1 0,0 -35,10
-                     z' />
-          </svg>
-          <svg className="svg3" viewBox='0 0 150 150'>
-            <path d='M 25,60
-                     a 20,20 1 0,0 0,80
-                     h 100
-                     a 20,20 1 0,0 0,-80
-                     a 10,10 1 0,0 -30,-20
-                     a 15,15 1 0,0 -70,20
-                     z' />
-          </svg>
-          <svg className="svg4" viewBox='0 0 105 105'>
-            <path d='M 25,60
-                     a 20,20 1 0,0 0,40
-                     h 50
-                     a 20,20 1 0,0 0,-40
-                     a 10,10 1 0,0 -15,-10
-                     a 15,15 1 0,0 -35,10
-                     z' />
-          </svg>
-          <svg className="svg5" viewBox='0 0 150 150'>
-            <path d='M 25,60
-                     a 20,20 1 0,0 0,80
-                     h 100
-                     a 20,20 1 0,0 0,-80
-                     a 10,10 1 0,0 -30,-20
-                     a 15,15 1 0,0 -70,20
-                     z' />
-          </svg>
-          <svg className="svg6" viewBox='0 0 105 105'>
-            <path d='M 25,60
-                     a 20,20 1 0,0 0,40
-                     h 50
-                     a 20,20 1 0,0 0,-40
-                     a 10,10 1 0,0 -15,-10
-                     a 15,15 1 0,0 -35,10
-                     z' />
-          </svg>
         </div>
       </nav>
       <Component {...pageProps} />
